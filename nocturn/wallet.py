4fc1f4adbc8e42f15e61d7149e3ee289b393add0
Suryanshsinh Sisodiya <suryanshsinhsisodiya@gmail.com>
2025-01-06 12:49:30 +0530

    added-testnet-support

--- nocturn/wallet.py
+++ nocturn/wallet.py
@@ -1,30 +1,29 @@
-import os
 from web3 import Web3
 from eth_account import Account
 from mnemonic import Mnemonic
 from bip32utils import BIP32Key
-
-RPC_ENDPOINTS = {
-    "ETH": "https://rpc.ankr.com/eth",
-    "POL": "https://rpc.ankr.com/polygon",
-    "BSC": "https://bsc-dataseed.binance.org/",
-}
-
-class InvalidMnemonicPhrase(Exception):
-    pass
-
-class InvalidPrivateKey(Exception):
-    pass
-
-class MismatchException(Exception):
-    pass
+from exceptions import *
 
 class Wallet:
+    RPC_ENDPOINTS = {
+        "testnet": {
+            "ETH": "https://rpc.sepolia.org",
+            "POL": "https://rpc-mumbai.maticvigil.com",
+            "BSC": "https://data-seed-prebsc-1-s1.binance.org:8545/",
+        }, 
+        "mainnet": {
+            "ETH": "https://rpc.ankr.com/eth",
+            "POL": "https://rpc.ankr.com/polygon",
+            "BSC": "https://bsc-dataseed.binance.org/",
+        }
+    }
+
     def __init__(self, mnemonic_phrase=None, private_key=None):
         self.mnemonic = None
         self.private_key = None
         self.address = None
         self.multichain = None
+        self.check_balance = self._instance_check_balance
         
         if mnemonic_phrase and private_key:
             raise MismatchException("Both mnemonic and private key cannot be passed at the same time.")
@@ -54,10 +53,13 @@ class Wallet:
     def _private_key_to_address(self, private_key):
         return Account.from_key(private_key).address
 
+    def _instance_check_balance(self, chain, testnet=False):
+        return Wallet.check_balance(self.address, chain)
+
     @staticmethod
-    def check_balance(address, chain):
-        if chain not in RPC_ENDPOINTS:
+    def check_balance(address, chain, testnet=False):
+        if chain not in Wallet.RPC_ENDPOINTS["testnet" if testnet else "mainnet"]:
             raise ValueError(f"Chain {chain} not supported.")
-        web3 = Web3(Web3.HTTPProvider(RPC_ENDPOINTS[chain]))
+        web3 = Web3(Web3.HTTPProvider(Wallet.RPC_ENDPOINTS["testnet" if testnet else "mainnet"][chain]))
         balance = web3.eth.get_balance(address)
         return web3.from_wei(balance, "ether")
